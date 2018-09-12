1cef56cb150d6f977e91caa1ab4dbe9fa3fdeacb
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-11 17:49:44 -0700

    Fortune Teller

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -23,19 +23,22 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
 
     JFrame frame = new JFrame();
 
-    int frameWidth = 255;
-    int frameHeight = 255;
+    int frameWidth = 250;
+    int frameHeight = 250;
 
     FortuneTeller() throws Exception {
    	 // 1. Choose an image for your fortune teller and put it in your default package
    	 fortuneTellerImage = ImageIO.read(getClass().getResource("fortune teller.png"));
    	 // 2. Adjust the frameWidth and frameHeight variables to fit your image nicely (doesn’t need a new line of code)
+   	 
    	 // 4. add a mouse listener to the frame
 
     }
 
     static void begin() {
    	 // 3. Welcome the user. Give them a hint for the secret location.
+    	
+    	JOptionPane.showMessageDialog(null, "Welcome!");
 
     }
 
